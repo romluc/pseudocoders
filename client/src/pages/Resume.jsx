9e4e4aef827dbb9f1725b2c41654f7df4fcbbf31
LeopoldoGurgel <leopoldogbp@gmail.com>
2024-01-08 13:56:33 -0500

    added resume file for download and updated resuma page

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -4,8 +4,30 @@ import '../styles/Resume.css'
 
 export default function Resume(){
     return(
-        <div>
+        <div className='fluid'>
             <h2 className="mb-5">Resume</h2>
+
+            <h3>Leopoldo Gurgel Barroso Pimentel</h3>
+            <div className='lead mb-2'>Web Developer</div>
+            <div className='row gap-0 column-gap-2 mb-4'>
+                <span className='col-lg-3 col-12'>(437) 299-6367</span>
+                <span className='col-lg-3 col-12'>leopoldogbp@gmail.com</span>
+                <span className='col-lg-3 col-12'><a href="https://www.linkedin.com/in/leopoldo-gurgel-24a404284/">Linkedin</a></span>
+            </div>
+
+            <a href="resume/Leopoldo'sResume.pdf" className='btn btn-outline-primary' download>Download the PDF file.</a>
+
+            <h4 className='mb-3 mt-4'>Carrer Objective</h4>
+            <p className='mb-4'>Self-motivated team member and avid learner, I want to as a Web Developer to apply my skills and understanding of both front and back-end web development to achieve client requirements and outmatch their expectations. Highly skilled in communication, collaboration and technical communication. Passionate about sharing knowledge and contributing to a positive team environment.</p>
+
+            <h4 className='mb-3'>Education</h4>
+            <ul className='mb-5'>
+                <li><strong>UofT SCS Coding Boot Camp, University of Toronto</strong>, Tonronto, On (2023)</li>
+                <li><strong>Responsive Web Design, FreeCodeCamp</strong> (2023)</li>
+                <li><strong>Javascript Coding Course, Mimo.org</strong>(2023)</li>
+                <li><strong>HTML Coding Course, Mimo.org</strong>(2023)</li>
+            </ul>
+
             <div id="proficienciesDiv">
                 <div>
                     <h4 className='mb-3'>Front-End Proficiencies</h4>
@@ -30,8 +52,9 @@ export default function Resume(){
                         <li>Restful API</li>
                         <li>GraphQl</li>
                     </ul>
-                </div>
+                </div>                
             </div>
+            <p>Fluent in Portuguese (native) and English.</p>
         </div>
     )
 }
